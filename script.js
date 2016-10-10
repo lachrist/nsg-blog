0d3a02f83b3acf946c68d26850c49c32b7b1ba88
lachrist <lachrist@vub.ac.be>
2016-10-10 13:59:13 +0200

    new docs

--- script.js
+++ script.js
@@ -23,9 +23,11 @@ window.onload = function () {
     "2016-10-15": "Gosselie I",
     "2016-10-16": "Gosselie II",
     "2016-10-22": "BCBW III",
-    "2016-10-23": "BCBW VI",
+    "2016-10-23": "BCBW IV",
     "2016-11-05": "Burny I",
     "2016-11-06": "Burny II",
+    "2016-11-12": "BE open 25m I"
+    "2016-11-13": "BE open 25m II"
     "2016-11-19": "Richard Anis I",
     "2016-11-20": "Richard Anis II"
   }
