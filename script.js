10eb4b1252de8b3f5db4a3b4a6a42af274108383
lachrist <lachrist@vub.ac.be>
2016-10-21 11:43:50 +0200

    startlist

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ window.onload = function () {
     "2016-10-23": "BCBW IV",
     "2016-11-05": "Burny I",
     "2016-11-06": "Burny II",
-    "2016-11-12": "BE open 25m I"
-    "2016-11-13": "BE open 25m II"
+    "2016-11-12": "BE open 25m I",
+    "2016-11-13": "BE open 25m II",
     "2016-11-19": "Richard Anis I",
     "2016-11-20": "Richard Anis II"
   }
