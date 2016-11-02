13056703fa7f063a298cdee680c0d045fab8f838
lachrist <lachrist@vub.ac.be>
2016-11-02 08:43:07 +0100

    cms

--- compets.js
+++ compets.js
@@ -3,8 +3,6 @@ var Compets = {
   "2016-10-09": "BCBW II",
   "2016-10-22": "BCBW III",
   "2016-10-23": "BCBW IV",
-  "2016-11-12": "BE Open 25m",
-  "2016-11-13": "BE Open 25m",
   "2016-11-19": "Richard Anis",
   "2016-11-20": "Richard Anis",
   "2016-11-25": "BCBW Relais I",
