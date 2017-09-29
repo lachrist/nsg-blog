865293545cd3a045dc128d451ad73e0ecc152cd5
lachrist <lachrist@vub.ac.be>
2017-09-29 09:03:31 +0200

    new season 2017

--- script.js
+++ script.js
@@ -109,8 +109,8 @@ module.exports = {
 module.exports = [
   "document/licence.pdf",
   "document/minima/2016-BCBW.pdf",
-  "document/minima/2016-BE-50m.pdf",
   "document/minima/2016-BE-25m.pdf",
+  "document/minima/2016-BE-50m.pdf",
   "document/minima/2017-FR-JEUNES-50m.pdf",
   "document/minima/2017-FR-OPEN-50m.pdf"
 ];
