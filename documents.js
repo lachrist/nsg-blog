0cc7ec8f4e5e656dd86358af809fca531cd3c4e1
lachrist <lachrist@vub.ac.be>
2018-01-29 18:03:57 +0100

    compet until jully 2018

--- documents.js
+++ documents.js
@@ -1,9 +1,9 @@
 module.exports = [
   "document/licence.pdf",
   "document/minima/2016-BCBW.pdf",
-  "document/minima/2016-BE-25m.pdf",
   "document/minima/2016-BE-50m.pdf",
   "document/minima/2017-BCBW.pdf",
+  "document/minima/2016-BE-25m.pdf",
   "document/minima/2017-FR-JEUNES-50m.pdf",
   "document/minima/2017-FR-OPEN-50m.pdf"
 ];
--- script.js
+++ script.js
@@ -59,7 +59,8 @@ module.exports = {
     "name": "BCBW II @Waterloo",
     "docs": [
       "Avant-programme Championnats BCBW II 21_10_2017_WN.pdf",
-      "Championnats BCBW 2017 II 21_10_2017.lxf"
+      "Championnats BCBW 2017 II 21_10_2017.lxf",
+      "Startlist Championnats BCBW II 21_10_2017.pdf"
     ]
   },
   "2017-10-22": {
@@ -88,7 +89,14 @@ module.exports = {
   },
   "2017-11-18": {
     "name": "Richard Anis @Blocry",
-    "docs": []
+    "docs": [
+      "Avant-programme Meeting de Richard Anis 18_11_2017.pdf",
+      "Avant-programme Meeting de Richard Anis 19_11_2017.pdf",
+      "Horaire Meeting de Richard Anis 2017.pdf",
+      "Meeting de Richard Anis 18_11_2017.lxf",
+      "NSG 18_11_2017.pdf",
+      "Startlist Meeting de Richard Anis 2017.pdf"
+    ]
   },
   "2017-11-19": {
     "name": "Richard Anis @Blocry",
@@ -100,10 +108,120 @@ module.exports = {
   },
   "2017-12-01": {
     "name": "BCBW Relais II @Poseidon",
-    "docs": []
+    "docs": [
+      "171201_nsg_confirmation.pdf",
+      "171201_rbp_relais2_resultats.pdf",
+      "171201_rbp_relais_resultats.lxf",
+      "NSG-Relais.lxf"
+    ]
   },
   "2017-12-29": {
     "name": "Derniere Chance @Mons",
+    "docs": []
+  },
+  "2018-01-07": {
+    "name": "Perpermans @Antwerpen",
+    "docs": [
+      "KAZSC_7_1_2018_Officieel voorprogramma Wisselsprintcriterium Frank Pepermans.pdf",
+      "KAZSC_7_1_2018_voorprogramma Wisselsprintcriterium Frank Pepermans.lxf"
+    ]
+  },
+  "2018-01-28": {
+    "name": "Meeting400m @Blocry",
+    "docs": [
+      "Avant-programme Meeting des 400m _ Boust_28_01_2018.lxf",
+      "Avant-programme Meeting des 400m _ Boust_28_01_2018.pdf",
+      "Exceptions dans les résultats.xls",
+      "Meeting des 400 m - eliminatoires Georges 28_01_2018.lxf",
+      "Meeting des 400 m - eliminatoires Georges 28_01_2018.pdf",
+      "Médaillés par épreuve.pdf",
+      "NSG 28_01_2018.pdf"
+    ]
+  },
+  "2018-03-03": {
+    "name": "MeetingSprints @Blocry",
+    "docs": [
+      "AP BOUST Meeting des Sprints 03_03_2018.pdf",
+      "AP BOUST Meeting des Sprints 04_03_ 2018.pdf"
+    ]
+  },
+  "2018-03-17": {
+    "name": "CNSW @Sportcity",
+    "docs": [
+      "Avant-programme 170318.pdf",
+      "fichier lenex 170318.lxf"
+    ]
+  },
+  "2018-04-27": {
+    "name": "Aernouts @Bastogne",
+    "docs": []
+  },
+  "2018-04-28": {
+    "name": "Aernouts @Bastogne",
+    "docs": []
+  },
+  "2018-04-29": {
+    "name": "Aernouts @Bastogne",
+    "docs": []
+  },
+  "2018-05-10": {
+    "name": "Glibert @Poseidon",
+    "docs": []
+  },
+  "2018-05-11": {
+    "name": "BEOpen",
+    "docs": []
+  },
+  "2018-05-12": {
+    "name": "BEOpen",
+    "docs": []
+  },
+  "2018-05-13": {
+    "name": "BEOpen",
+    "docs": []
+  },
+  "2018-05-19": {
+    "name": "EuroMeeting @Embourg",
+    "docs": []
+  },
+  "2018-05-20": {
+    "name": "EuroMeeting @Embourg",
+    "docs": []
+  },
+  "2018-05-21": {
+    "name": "GrandPrix @Mouscron",
+    "docs": []
+  },
+  "2018-07-20": {
+    "name": "BEWE1",
+    "docs": []
+  },
+  "2018-07-21": {
+    "name": "BEWE1",
+    "docs": []
+  },
+  "2018-07-22": {
+    "name": "BEWE1",
+    "docs": []
+  },
+  "2018-07-24": {
+    "name": "Zadwornyght @ChaudFontaine",
+    "docs": []
+  },
+  "2018-07-25": {
+    "name": "Nocturne200m @Embourg",
+    "docs": []
+  },
+  "2018-07-27": {
+    "name": "BEWE2",
+    "docs": []
+  },
+  "2018-07-28": {
+    "name": "BEWE2",
+    "docs": []
+  },
+  "2018-07-29": {
+    "name": "BEWE2",
     "docs": []
   }
 };
@@ -111,9 +229,9 @@ module.exports = {
 module.exports = [
   "document/licence.pdf",
   "document/minima/2016-BCBW.pdf",
-  "document/minima/2016-BE-25m.pdf",
   "document/minima/2016-BE-50m.pdf",
   "document/minima/2017-BCBW.pdf",
+  "document/minima/2016-BE-25m.pdf",
   "document/minima/2017-FR-JEUNES-50m.pdf",
   "document/minima/2017-FR-OPEN-50m.pdf"
 ];
